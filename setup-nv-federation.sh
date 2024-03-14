841ffcf5cea2f567a20cf47be552600cea8ae428
Giovanni Lo Vecchio <glovecchio@MacBook-Pro.homenet.telecomitalia.it>
2024-03-14 10:22:06 +0100

    Changed wording from master to controller

--- setup-nv-federation.sh
+++ setup-nv-federation.sh
@@ -14,8 +14,8 @@ curl -k -H "Content-Type: application/json" -d '{"password": {"username": "admin
 PRIMARY_CLUSTER_ADMIN_PWD_TOKEN=`cat ./primary_cluster_admin_token.json | jq -r '.token.token'`
 echo
 
-echo "Promotion of the primary cluster to Master."
-curl -k -H "Content-Type: application/json" -H "X-Auth-Token: $PRIMARY_CLUSTER_ADMIN_PWD_TOKEN" -d '{"master_rest_info": {"port": 11443, "server": "'$PRIMARY_CLUSTER_FEDMASTER_IP'"}, "name": "master"}' "https://$PRIMARY_CLUSTER_FEDMANAGED_IP:10443/v1/fed/promote" > /dev/null 2>&1
+echo "Promotion of the primary cluster to Controller."
+curl -k -H "Content-Type: application/json" -H "X-Auth-Token: $PRIMARY_CLUSTER_ADMIN_PWD_TOKEN" -d '{"master_rest_info": {"port": 11443, "server": "'$PRIMARY_CLUSTER_FEDMASTER_IP'"}, "name": "controller"}' "https://$PRIMARY_CLUSTER_FEDMANAGED_IP:10443/v1/fed/promote" > /dev/null 2>&1
 sleep 6
 echo
 
@@ -24,7 +24,7 @@ curl -k -H "Content-Type: application/json" -d '{"password": {"username": "admin
 PRIMARY_CLUSTER_ADMIN_PWD_TOKEN=`cat ./primary_cluster_admin_token.json | jq -r '.token.token'`
 echo
 
-echo "Retrieve the join_token from the Master cluster."
+echo "Retrieve the join_token from the Controller cluster."
 curl -k -H "Content-Type: application/json" -H "X-Auth-Token: $PRIMARY_CLUSTER_ADMIN_PWD_TOKEN" "https://$PRIMARY_CLUSTER_FEDMANAGED_IP:10443/v1/fed/join_token" > /dev/null 2>&1 > ./primary_cluster_join_token.json
 cat ./primary_cluster_join_token.json | jq -c .
 PRIMARY_CLUSTER_JOIN_TOKEN=`cat ./primary_cluster_join_token.json | jq -r '.join_token'`
@@ -35,12 +35,12 @@ curl -k -H "Content-Type: application/json" -d '{"password": {"username": "admin
 SECONDARY_CLUSTER_ADMIN_PWD_TOKEN=`cat ./secondary_cluster_admin_token.json | jq -r '.token.token'`
 echo
 
-echo "Join the remote cluster (now WORKER) to the Master."
+echo "Join the remote cluster (now WORKER) to the Controller."
 curl -k -H "Content-Type: application/json" -H "X-Auth-Token: $SECONDARY_CLUSTER_ADMIN_PWD_TOKEN" -d '{"join_token": "'$PRIMARY_CLUSTER_JOIN_TOKEN'", "name": "worker", "joint_rest_info": {"port": 10443, "server": "'$SECONDARY_CLUSTER_FEDMANAGED_IP'"}}' "https://$SECONDARY_CLUSTER_FEDMANAGED_IP:10443/v1/fed/join" > /dev/null 2>&1
 sleep 9
 echo
 
-echo "Check the federation status on the Master cluster."
+echo "Check the federation status on the Controller cluster."
 curl -k -H "Content-Type: application/json" -H "X-Auth-Token: $PRIMARY_CLUSTER_ADMIN_PWD_TOKEN" "https://$PRIMARY_CLUSTER_FEDMANAGED_IP:10443/v1/fed/member" > /dev/null 2>&1 > ./fed_member.json
 cat ./fed_member.json | jq -c .
 echo

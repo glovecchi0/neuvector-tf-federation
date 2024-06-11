e17dfd8da21edde8efb4b64c59e419e859cfbbe3
Giovanni Lo Vecchio <glovecchio@MacBook-Pro.homenet.telecomitalia.it>
2024-06-11 16:53:46 +0200

    All Provider versions updated - GoogleCloud/AWS

--- remote/eks/outputs.tf
+++ remote/eks/outputs.tf
@@ -4,11 +4,11 @@ output "neuvector-webui-url" {
 }
 
 output "neuvector-svc-controller-fed-managed" {
-  value       = "${data.kubernetes_service.neuvector-svc-controller-fed-managed.status.0.load_balancer.0.ingress[0].hostname}"
+  value       = data.kubernetes_service.neuvector-svc-controller-fed-managed.status.0.load_balancer.0.ingress[0].hostname
   description = "NeuVector fed-managed IP"
 }
 
 output "neuvector-svc-controller-fed-master" {
-  value       = "${data.kubernetes_service.neuvector-svc-controller-fed-master.status.0.load_balancer.0.ingress[0].hostname}"
+  value       = data.kubernetes_service.neuvector-svc-controller-fed-master.status.0.load_balancer.0.ingress[0].hostname
   description = "NeuVector fed-master IP"
 }
